f8ee57edc2d1bf0a49ffa04a00854fa2b9281df0
Selkie-13 <quentin.barbier89@gmail.com>
2020-05-06 00:20:51 +0200

    200506

--- R/main.R
+++ R/main.R
@@ -8,67 +8,82 @@ GoFo <- function(){
 }
 
 #`@export
-theorique.prob <- function(x, law="Poisson",br=NULL){
-  if(is.null(br)){
-    br <- seq(round(min(x))-0.5,round(max(x))+0.5,1)
-  }
-  h <- hist(x, plot=FALSE, freq = TRUE,breaks=br)
-  mid <- h$mid
-  dens <- h$density
-  p <- abs(ceiling(mid))
+theorique.prob <- function(x, law="Poisson"){
+  p <- density(x)$x
   if(law=="Uniforme"){
     prob <- rep(1/length(p),length(p))
-  }
-  if(law="Exponentille"){
+  } else if(law=="Exponentielle"){
     res <- fitdist(x,"exp")
-    prob <- dexp(p,res@estimate)
-  }
-  if(law=="Poisson"){
+    prob <- dexp(p,res$estimate)
+  } else if(law=="Poisson"){
     res <- fitdist(x,"pois")
-    prob <- dpois(p,lambda=res$estimate)
-  }
-  if(law=="Normale"){
+    prob <- dpois(ceiling(p),lambda=as.numeric(res$estimate))
+  } else if(law=="Normale"){
     res <- fitdist(x,"norm")
     prob <- dnorm(p,res$estimate[[1]],res$estimate[[2]])
-  }
-  if(law=="LogNormale"){
+  } else if(law=="LogNormale"){
     res <- fitdist(x,"lnorm")
     prob <- dlnorm(p,res$estimate[[1]],res$estimate[[2]])
-  }
-  if(law=="Gamma"){
+  } else if(law=="Gamma"){
     res <- fitdistr(x,"gamma")
     prob <- dgamma(p,res$estimate[[1]],res$estimate[[2]])
-  }
-  if(law=="NegBinomial"){
+  } else if(law=="NegBinomial"){
     res <- fitdistr(x,"nbinom")
     prob <- dnbinom(p,res$estimate[[1]],res$estimate[[2]])
+  }
+  else {
+    prob <- NULL
   }
   return(prob)
 }
 
 #`@export
-test.adjust <- function(x,test="chi2",prob){
-  if(is.null(br)){
-    br <- seq(round(min(x))-0.5,round(max(x))+0.5,1)
-  }
-  h <- hist(x, plot=FALSE, freq = TRUE,breaks=br)
-  mid <- h$mid
-  dens <- h$density
-  p <- abs(ceiling(mid))
-
+test.adjust <- function(x,test="ks",prob){
+  p <- density(x)$y
+  prob <- prob/sum(prob)
   if(test=="chi2"){
-    pval <- chisq.test(x,p=prob)$p.value
+    pval <- chisq.test(density(x)$y,p=prob)$p.value
   }
-
   if(test=="ks"){
     pval <- ks.test(x, y=prob)$p.value
   }
-
   if(test=="sw"){
     pval <- shapiro.test(x)$p.value
   }
-
+  return(pval)
 }
 
+#`@export
+plot.ecdf.adjust <- function(x,probs){
+  a <- density(x)$x
+  par(mar=c(2,2,2,2))
+  plot(ecdf(x),main="",col="gray")
+  colors <- c("red","blue","green","purple","orange")
+  for(i in c(1:length(probs))){
+    p <- probs[[i]]/sum(probs[[i]])
+    lines(x=sort(a),y=cumsum(p),col=colors[[i]],lty=2,lwd=2)
+  }
+}
 
+#`@export
+plot.hist.adjust <- function(x, probs){
+  a <- density(x)$x
+  b <- density(x)$y
+  par(mar=c(2,2,2,2))
+  plot(a,b,type="h",col="gray",main=NULL,xlab=NULL,ylab=NULL,ylim=c(0,max(b)*1.33))
+  colors <- rainbow(length(probs))
+  colors <- c("red","blue","green","purple","orange")
+  for(i in c(1:length(probs))){
+    lines(x=a,y=probs[[i]],col=colors[[i]],lty=2,lwd=2)
+  }
+}
 
+#`@export
+demo <- function(){
+  x <- rpois(1000,50)
+  laws <- c("Uniforme","Normale","Gamma","Poisson","Exponentielle")
+  probs <- lapply(laws,function(i){theorique.prob(x,law=i)})
+  names(probs) <- laws
+  pvals <- lapply(probs,function(i){test.adjust(x,test="chi2",prob)})
+  names(pvals) <- laws
+}
